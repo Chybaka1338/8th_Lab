40b79c7ec93b6edcacf198559b1dbe1d629abbad
Chybaka1337 <zommer.03@gmail.com>
2023-03-19 22:00:56 +0300

    Добавьте файлы проекта.

---
+++ 8th_Lab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace _8th_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1.Run();
+            Console.ReadKey();
+        }
+
+        static void Task2()
+        {
+
+        }
+    }
+}
---
+++ 8th_Lab/Task1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8th_Lab
+{
+    internal class Task1
+    {
+        static string text;
+        static int total;
+        static Dictionary<Char, int> dict;
+        public static void Run()
+        {
+            Console.WriteLine("введите текст");
+            text = Console.ReadLine().ToLower();
+            SetTotalLetters();
+            SetStatistic();
+            PrintStatistic();
+            Console.WriteLine();
+        }
+
+        static void SetTotalLetters()
+        {
+            total = 0;
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsLetter(text[i]))
+                    total++;
+            }
+        }
+
+        static void SetStatistic()
+        {
+            dict = new Dictionary<Char, int>();
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (text[i] >= 0x0430 && text[i] <= 0x044F)
+                {
+                    if (!dict.ContainsKey(text[i]))
+                        dict[text[i]] = 1;
+                    else
+                        dict[text[i]]++;
+                }
+            }
+        }
+
+        static void PrintStatistic()
+        {
+            foreach(var pair in dict)
+            {
+                double frequency = (double)pair.Value / total;
+                Console.WriteLine("symbol: {0} {1:0.#}%", pair.Key, pair.Value);
+            }
+        }
+    }
+}
